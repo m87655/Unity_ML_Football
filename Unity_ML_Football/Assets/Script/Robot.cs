86fa1aee24763668f4ebb6c77e48f4ea3a4226a4
m87655 <55180804+m87655@users.noreply.github.com>
2020-06-18 16:23:28 +0800

    finish

--- Unity_ML_Football/Assets/Script/Robot.cs
+++ Unity_ML_Football/Assets/Script/Robot.cs
@@ -8,11 +8,11 @@ public class Robot : Agent
     public float speed = 10;
 
     /// <summary>
-    /// 機器人剛體
+    /// 機器人鋼體
     /// </summary>
     private Rigidbody rigRobot;
     /// <summary>
-    /// 足球剛體
+    /// 足球鋼體
     /// </summary>
     private Rigidbody rigBall;
 
@@ -23,42 +23,40 @@ public class Robot : Agent
     }
 
     /// <summary>
-    /// 事件開始時：重新設定機器人與足球位置
+    /// 事件開始時:重新設定機器人與足球位置
     /// </summary>
     public override void OnEpisodeBegin()
     {
-        // 重設剛體加速度與角色加速度
+        //重設鋼體的加速度與角度加速度
         rigRobot.velocity = Vector3.zero;
         rigRobot.angularVelocity = Vector3.zero;
         rigBall.velocity = Vector3.zero;
         rigBall.angularVelocity = Vector3.zero;
 
-        // 隨機機器人位置
-        Vector3 posRobot = new Vector3(Random.Range(-1f, 1f), 0.1f, Random.Range(-1f, 0f));
+        //隨機機器人位置
+        Vector3 posRobot = new Vector3(Random.Range(-1f, 1f), 0.1f, Random.Range(0f, 0.5f));
         transform.position = posRobot;
 
         //隨機足球位置
-        Vector3 posBall = new Vector3(Random.Range(-0.5f, 0.5f), 0.1f, Random.Range(1f, 2f));
+        Vector3 posBall = new Vector3(Random.Range(-0.5f, 0.5f), 0.1f, Random.Range(1f, 1.5f));
         rigBall.position = posBall;
+        //球尚未進入球門
 
-        //足球尚未進入球門
         Ball.complete = false;
     }
-
     /// <summary>
     /// 收集觀測資料
     /// </summary>
     public override void CollectObservations(VectorSensor sensor)
     {
-        //加入觀測資料：機器人、足球座標、機器人加速度 X、Z
+        //加入觀測資料:機器人與足球位置、機器人加速度 X、Z
         sensor.AddObservation(transform.position);
         sensor.AddObservation(rigBall.position);
         sensor.AddObservation(rigRobot.velocity.x);
         sensor.AddObservation(rigRobot.velocity.z);
     }
-
     /// <summary>
-    /// 動作：控制機器人與回饋
+    /// 動作:控制機器人與回饋
     /// </summary>
     public override void OnActionReceived(float[] vectorAction)
     {
@@ -68,19 +66,26 @@ public class Robot : Agent
         control.z = vectorAction[1];
         rigRobot.AddForce(control * speed);
 
-        //球進入球門，成功：加1分並結束
+        //球進球門:成功:加一分並結束
         if (Ball.complete)
         {
             SetReward(1);
             EndEpisode();
         }
 
-        //機器人或足球掉到地板下方
+        //機器人或球掉到地板下:失敗:扣一分並結束
         if (transform.position.y < 0 || rigBall.position.y < 0)
         {
             SetReward(-1);
             EndEpisode();
         }
     }
-
+    /// <summary>
+    /// 探索:讓開發者測試環境
+    /// </summary>
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
+    }
 }
